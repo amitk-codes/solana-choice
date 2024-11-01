d38ba9687320fddc3ee8d7e4641dd2a95ee8c463
Amit Kumar <amitkcodes@gmail.com>
2024-11-01 21:25:43 +0530

    added a test for initializing choice account

--- tests/solana_choice.ts
+++ tests/solana_choice.ts
@@ -59,4 +59,67 @@ describe("solana_choice", () => {
     assert.equal(fetchPollAccount.startDate.toNumber(), demoStartDate)
     assert.equal(fetchPollAccount.endDate.toNumber(), demoEndDate)
   });
+
+  it("Initializes the Choice Account", async () => {
+
+    // First initializing the poll account
+    const demoKeypair = web3.Keypair.generate();
+    await fundWallet(demoKeypair.publicKey)
+
+    const [pollAccountBump] = web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("poll"), new anchor.BN(1).toArrayLike(Buffer, "le", 8), demoKeypair.publicKey.toBuffer()],
+      program.programId
+    )
+
+    const demoPollId = 1;
+    const demoDescription = "test-description";
+    const demoStartDate = 1727760548;
+    const demoEndDate = 1733030948;
+
+    const pollAccounts = {
+      signer: demoKeypair.publicKey,
+      poll_account: pollAccountBump,
+      system_program: web3.SystemProgram.programId
+    }
+
+    await program.methods
+      .initializePoll(
+        new anchor.BN(demoPollId),
+        demoDescription,
+        new anchor.BN(demoStartDate),
+        new anchor.BN(demoEndDate)
+      )
+      .accounts(pollAccounts)
+      .signers([demoKeypair])
+      .rpc();
+
+
+    // Now running actual test by using above poll account
+
+    const demoChoice = "test-choice"
+
+    const [choiceAccountBump] = await web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("choice"), new anchor.BN(1).toArrayLike(Buffer, "le", 8), Buffer.from(demoChoice), demoKeypair.publicKey.toBuffer()],
+      program.programId
+    )
+
+    const accounts = {
+      signer: demoKeypair.publicKey,
+      choice_account: choiceAccountBump,
+      system_program: web3.SystemProgram.programId
+    }
+
+    await program.methods
+      .initializeChoice(
+        new anchor.BN(1),
+        demoChoice
+      )
+      .accounts(accounts)
+      .signers([demoKeypair])
+      .rpc()
+
+    const fetchChoiceAccount = await program.account.choiceAccount.fetch(choiceAccountBump);
+    assert.equal(fetchChoiceAccount.choiceName, demoChoice);
+    assert.equal(fetchChoiceAccount.choiceVotes.toNumber(), 0)
+  })
 });
